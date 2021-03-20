c1a18c7d8987f0e2bcf0d2590ecaac9a202803ed
odedron100 <odedron84@gmail.com>
2021-03-20 16:11:02 +0200

    Add design to packge price

--- src/components/package-price.vue
+++ src/components/package-price.vue
@@ -1,14 +1,14 @@
 <template>
-  <section class="package-price-container">
-    <header>
+  <section class="package-price-container container">
+    <header class="price-container">
       <h3>package price</h3>
-      <p>{{gig.price}} $ </p>
+      <p class="price">{{gig.price}} $ </p>
     </header>
-    <p class="about">{{gig.about}}</p>
-    <div class="delivery-time">Delivery time: {{gig.deliveryTime}}</div>
+    <p class="package-about">{{gig.about}}</p>
+    <div class="delivery-time"><i class="far fa-clock"></i>{{gig.deliveryTime}}</div>
     <ul class="extras">
       <li class="extra" v-for="extra in gig.extras" :key="extra">
-        {{extra}}
+        <span><i class="fas fa-check"></i></span> {{extra}}
       </li>
     </ul>
     <button class="checkout-button" @click="onCheckout">checkout</button>
