8e55875589ea35f44c8f67dac00bcafad951cd00
odedron100 <odedron84@gmail.com>
2021-03-23 12:45:42 +0200

    Fix stars rating in reviews

--- src/components/package-price.vue
+++ src/components/package-price.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="package-price-container container">
     <header class="price-container">
-      <h3>package price</h3>
+      <h3>Package Price</h3>
       <p class="price">$ {{gig.price}}</p>
     </header>
     <p class="package-about">{{gig.about}}</p>
-    <div class="delivery-time"><i class="far fa-clock"></i> {{gig.deliveryTime}} Days delivery</div>
+    <div class="delivery-time"><i class="far fa-clock"></i> {{gig.deliveryTime}}</div>
     <ul class="extras">
       <li class="extra" v-for="extra in gig.extras" :key="extra">
         <span><i class="fas fa-check"></i></span> {{extra}}
