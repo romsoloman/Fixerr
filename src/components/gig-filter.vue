4f2f6d5bac73312dfad9c6dbfadd32b8a150b66c
Rom Soloman <romsoloman19@gmail.com>
2021-03-25 15:32:26 +0200

    add fixed to gig app

--- src/components/gig-filter.vue
+++ src/components/gig-filter.vue
@@ -4,75 +4,87 @@
       <div class="search-icon">
         <i class="fas fa-search"></i>
       </div>
-      <input type="text" v-model="filterBy.name" class="search-input" />
+      <input
+        type="text"
+        v-model="filterBy.name"
+        class="search-input"
+        placeholder="Find Services"
+      />
       <button @click="setFilter" class="search-button">Search</button>
     </div>
-    <div class="flex align-center justify-center border-filter price-search">
-      <h5 class="budget">Budget</h5>
-      <button class="arrow-btn" @click="isBudgClicked = !isBudgClicked">
-        <span><i class="fas fa-arrow-down"></i></span>
-      </button>
-      <div v-if="isBudgClicked" class="flex column floating-menu">
-        <div class="flex min-max">
-          <div class="min-price">
-            <h5>Min.</h5>
-            <input
-              type="number"
-              placeholder="Any"
-              min="0"
-              max="5000"
-              v-model.number="filterBy.price.minPrice"
-            />
+    <div class="flex filter-actions">
+      <div class="flex align-center justify-center border-filter price-search">
+        <h5 class="budget">Budget</h5>
+        <button class="arrow-btn" @click="isBudgClicked = !isBudgClicked">
+          <span><i class="fas fa-arrow-down"></i></span>
+        </button>
+        <div v-if="isBudgClicked" class="flex column floating-menu">
+          <div class="flex min-max">
+            <div class="min-price">
+              <h5>Min.</h5>
+              <input
+                type="number"
+                placeholder="Any"
+                min="0"
+                max="5000"
+                v-model.number="filterBy.price.minPrice"
+              />
+            </div>
+            <div class="max-price">
+              <h5>Max.</h5>
+              <input
+                type="number"
+                placeholder="Any"
+                min="0"
+                max="5000"
+                v-model.number="filterBy.price.maxPrice"
+              />
+            </div>
           </div>
-          <div class="max-price">
-            <h5>Max.</h5>
-            <input
-              type="number"
-              placeholder="Any"
-              min="0"
-              max="5000"
-              v-model.number="filterBy.price.maxPrice"
-            />
+          <div class="flex button-apply">
+            <button @click="setFilter">Apply</button>
           </div>
-        </div>
-        <div class="flex button-apply">
-          <button @click="setFilter">Apply</button>
         </div>
       </div>
-    </div>
-    <div class="flex align-center justify-center border-filter rating-search">
-      <h5>Rating</h5>
-      <button class="arrow-btn" @click="isRateClicked = !isRateClicked">
-        <span><i class="fas fa-arrow-down"></i></span>
-      </button>
-      <div v-if="isRateClicked" class="floating-menu">
-        <div class="flex align-center rating-value">
-          <input
-            type="range"
-            v-model.number="filterBy.rating"
-            min="1"
-            max="5"
-          />
-          <span>Rating : {{ filterBy.rating }}</span>
-        </div>
-        <div class="flex button-apply">
-          <button @click="setFilter">Apply</button>
+      <div class="flex align-center justify-center border-filter rating-search">
+        <h5>Rating</h5>
+        <button class="arrow-btn" @click="isRateClicked = !isRateClicked">
+          <span><i class="fas fa-arrow-down"></i></span>
+        </button>
+        <div v-if="isRateClicked" class="floating-menu">
+          <div class="flex align-center rating-value">
+            <input
+              type="range"
+              v-model.number="filterBy.rating"
+              min="1"
+              max="5"
+            />
+            <span>Rating : {{ filterBy.rating }}</span>
+          </div>
+          <div class="flex button-apply">
+            <button @click="setFilter">Apply</button>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="flex align-center justify-center border-filter level">
-      <h5>Seller level</h5>
-      <button class="arrow-btn" @click="isLevelClicked = !isLevelClicked">
-        <span><i class="fas fa-arrow-down"></i></span>
-      </button>
-      <div v-if="isLevelClicked" class="floating-menu">
-        <div class="flex align-center justify-center level-value">
-          <input type="range" v-model.number="filterBy.level" min="1" max="5" />
-          <span>Level : {{ filterBy.level }}</span>
-        </div>
-        <div class="flex button-apply">
-          <button @click="setFilter">Apply</button>
+      <div class="flex align-center justify-center border-filter level">
+        <h5>Seller level</h5>
+        <button class="arrow-btn" @click="isLevelClicked = !isLevelClicked">
+          <span><i class="fas fa-arrow-down"></i></span>
+        </button>
+        <div v-if="isLevelClicked" class="floating-menu">
+          <div class="flex align-center justify-center level-value">
+            <input
+              type="range"
+              v-model.number="filterBy.level"
+              min="1"
+              max="5"
+            />
+            <span>Level : {{ filterBy.level }}</span>
+          </div>
+          <div class="flex button-apply">
+            <button @click="setFilter">Apply</button>
+          </div>
         </div>
       </div>
     </div>
