cc95a363730652e33b6a293fa4b732e1b514b505
Rom Soloman <romsoloman19@gmail.com>
2021-03-30 22:47:26 +0300

    fix

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 import { router } from '@/router'
 
-const BASE_URL = (process.env.NODE_ENV !== 'development')
+const BASE_URL = process.env.NODE_ENV === 'production'
     ? '/api/'
     : '//localhost:3031/api/'
 
